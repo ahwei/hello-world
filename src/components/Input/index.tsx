ac891b99e6994fb0940ede6f312a2f944471d600
AhWeiHENGuEi <aa9440307@gmail.com>
2024-11-16 18:42:34 +0800

    feat: add custom Input component with outlined variant and styling adjustments

---
+++ src/components/Input/index.tsx
@@ -0,0 +1,31 @@
+import type { TextFieldProps } from '@mui/material';
+import { TextField } from '@mui/material';
+
+const Input = ({ sx, ...props }: TextFieldProps) => {
+  return (
+    <TextField
+      {...props}
+      variant="outlined"
+      sx={[
+        {
+          height: 60,
+          '& .MuiOutlinedInput-root': {
+            color: 'common.white',
+            '& fieldset': {
+              borderColor: 'grey.300',
+            },
+            '&:hover fieldset': {
+              borderColor: 'grey.300',
+            },
+            '&.Mui-focused fieldset': {
+              borderColor: 'secondary.main',
+            },
+          },
+        },
+        ...(Array.isArray(sx) ? sx : [sx]),
+      ]}
+    />
+  );
+};
+
+export default Input;
